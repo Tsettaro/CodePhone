c71a4257f5ecaba1019a4b7968ca4be41a3a0f0c
Artur <43388317+Tsettaro@users.noreply.github.com>
2024-03-09 02:11:06 +0300

    Master (#3)
    * Make new user when start bot (and even when you clean your directories in server)
    
    * Добавлено ограничение по количеству запросов от пользователя
    
    * Определение даты и времени по timestamp выделено в отдельную функцию
    
    * Реализация сохранения timestamp пользователя для ограничения количества запросов
    
    * Merge pull request #2 from Tsettaro:debug
    
    Слияние дебага
    
    * The start of recognise audio
    
    * Recognize is done!

--- teryphone.py
+++ teryphone.py
@@ -1,34 +1,39 @@
 from py_data import bot
 import user_func as js, os
+from guard import rate_limit
+from VTT import recognize
 # Define the start command handler function
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.reply_to(message, "Hello! I am your bot.")
-
-# Define the help command handler function
-@bot.message_handler(commands=['help'])
-def help_message(message):
-    bot.reply_to(message, "Help! I need somebody.")
+    js.check_and_add_user(message.from_user.id, str(message.from_user.first_name), message.date)
 
 # Define the echo function to repeat messages
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
-    bot.send_message(message.chat.id, str(message.from_user.first_name + " " + message.from_user.last_name))
-
+    wait = rate_limit(message)
+    if wait == 0:
+        bot.reply_to(message, message.text)
+        bot.send_message(message.chat.id, str(message.from_user.first_name))
+    else:
+        bot.reply_to(message, f'Извините, но нужно подождать {round(wait)} секунд после использования предыдущей команды.')
 @bot.message_handler(content_types=['voice'])
 def handle_voice_message(message):
-    bot.reply_to(message, "Вы прислали голосовое сообщение. Да начнётся дешифрование!")
-    voice = bot.get_file(message.voice.file_id)
-    js.check_and_add_user(message.from_user.id, str(message.from_user.first_name + " " + message.from_user.last_name), message.date)
-    js.save_audio(message.from_user.id, voice, message.date)
+    wait = rate_limit(message)
+    if wait == 0:
+        bot.reply_to(message, "Вы прислали голосовое сообщение. Да начнётся дешифрование!")
+        js.check_and_add_user(message.from_user.id, str(message.from_user.first_name), message.date)
+        wav = js.save_audio(message.from_user.id, bot.get_file(message.voice.file_id), message.date)
+        bot.reply_to(message, f"Расшифровка голосового сообщения: \n{recognize(wav)}")
+    else:
+        bot.reply_to(message, f'Извините, но нужно подождать {round(wait)} секунд после использования предыдущей команды.')
 
 # Main function to start the bot
 def main():
     if not os.path.exists('key.txt'):
         print("ERROR! Key doesn't exist! Please, contact with @useless_acc fro futher information")
         exit()
-    bot.polling()
+    bot.polling(none_stop=True)
 
 if __name__ == '__main__':
     main()
--- user_func.py
+++ user_func.py
@@ -2,11 +2,13 @@ import json, os
 from pathlib import Path
 from datetime import datetime
 from py_data import bot
-def check_and_add_user(user_id, username, timestamp):
-    date_of_start = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+import subprocess
+import wave
+def from_timestamp_to_date(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
-    if not os.path.exists('user_sectret_data'):
-        os.makedirs('user_sectret_data')
+def check_and_add_user(user_id, username, timestamp):
+    date_of_start = from_timestamp_to_date(timestamp)
     
     if not os.path.exists('user_sectret_data/user.json') or os.path.getsize('user_sectret_data/user.json') == 0:
         with open('user_sectret_data/user.json', 'w') as file:
@@ -19,18 +21,14 @@ def check_and_add_user(user_id, username, timestamp):
             data[str(user_id)] = {
                 "user": username,
                 "date_of_start": date_of_start,
-                "count_of_voice_message": 1,
+                "count_of_voice_message": 0,
             }
-        else:
-            # Обновить информацию о существующем пользователе
-            data[str(user_id)]["count_of_voice_message"] += 1
-        
         # Записать обновленные данные обратно в файл
         with open('user_sectret_data/user.json', 'w') as file:
             json.dump(data, file)
 
 def save_audio(user_id, audio, timestamp):
-    date_of_start = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d-%H:%M:%S")
+    date_of_start = from_timestamp_to_date(timestamp)
     
     if not os.path.exists('user_voice_messages'):
         os.makedirs('user_voice_messages')
@@ -45,10 +43,15 @@ def save_audio(user_id, audio, timestamp):
         os.makedirs(directory_path)
     # Define a list of audio file extensions
     voice = bot.get_file(audio.file_id)
-
-    with open(directory_path + '/' +(str(date_of_start).replace(':','_').replace('-','_') + '.ogg'), 'wb') as file:
+    filename_path = directory_path + '/' +(str(date_of_start).replace(':','_').replace('-','_'))
+    
+    with open(filename_path+'.oga', 'wb') as file:
         file.write(bot.download_file(voice.file_path))
-    # Count the audio files using a generator expression
-    #audio_file_count = sum(1 for file in directory_path.glob('*') if file.suffix in audio_extensions)
-    
-    
+    subprocess.run(['ffmpeg', '-i', filename_path+'.oga', filename_path+'.wav'])
+    os.remove(filename_path+'.oga')
+    with open('user_sectret_data/user.json', 'r') as file:
+        data = json.load(file)
+        data[str(user_id)]["count_of_voice_message"] += 1
+        with open('user_sectret_data/user.json', 'w') as file:
+            json.dump(data, file)
+    return filename_path+'.wav'
